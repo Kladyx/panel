264a3d909d841a51cc12b607b996c5a96f8b6c67
Kladyx <158815312+Kladyx@users.noreply.github.com>
2026-06-27 13:30:34 +0200

    Create spust_equalizer.bat

---
+++ spust_equalizer.bat
@@ -0,0 +1,12 @@
+@echo off
+title KLADYX Audio Analyzer
+cd /d C:\Users\klady\Desktop
+echo.
+echo ========================================
+echo  KLADYX AUDIO ANALYZER - Stereo EQ
+echo ========================================
+echo.
+python audio_analyzer.py
+echo.
+echo Script ukoncen. Stiskni libovolnou klavesu...
+pause
